a4f88334f31fa254f3509e4ec58b73af25c0b656
virusmarathe <virusmarathe@gmail.com>
2021-01-13 22:05:44 -0800

    starter lobby connection screen

---
+++ Assets/Scripts/UI/LobbyUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LiteNetLib;
+
+public class LobbyUIManager : MonoBehaviour
+{
+    [SerializeField] GameObject LobbyUIParent;
+    [SerializeField] InputField IPField;
+    [SerializeField] Text DebugText;
+
+    private void Awake()
+    {
+        IPField.text = NetUtils.GetLocalIp(LocalAddrType.IPv4);
+    }
+
+    protected void OnDisconnected(DisconnectInfo info)
+    {
+        DebugText.text = info.Reason.ToString();
+    }
+
+    public void OnHostClicked()
+    {
+        LobbyUIParent.SetActive(false);
+    }
+
+    public void OnConnectClicked()
+    {
+        LobbyUIParent.SetActive(false);
+    }
+}
